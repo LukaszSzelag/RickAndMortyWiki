946d50fdf3bf7e6ce12f74a387bea35ac510bc1a
Łukasz Szeląg <lukasz.szelag95@gmail.com>
2026-06-03 17:13:44 +0200

    Initial Commit

---
+++ RickAndMortyWiki/RickAndMortyWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyWikiApp.swift
+//  RickAndMortyWiki
+//
+//  Created by Łukasz Szeląg on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
